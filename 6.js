51990696454a4d4eb16630c681e122c730b0ff62
Alexander Makarof <makarofalex@gmail.com>
2020-04-26 18:44:12 +0300

    added some algs

---
+++ 6.js
@@ -0,0 +1,7 @@
+function deter({ size, redness }) {
+  const fruit = [
+    { name: "grape", size: 1, redness: 0 },
+    { name: "orange", size: 1, redness: 0 },
+    { name: "grape", size: 1, redness: 0 }
+  ];
+}
---
+++ 7.js
@@ -0,0 +1,42 @@
+// const evenNumbersFilter = (arr) => {
+//   return arr.filter((x) => x > 224);
+// };
+
+// const result = evenNumbersFilter([1321, 3, 1, 43, 432]);
+
+// console.log(result);
+
+// const arraySearchEven = (array) => {
+//   let result = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 === 0) {
+//       result.push(array[i]);
+//     } else continue;
+//   }
+//   console.log(result);
+//   return result;
+// };
+
+// arraySearchEven([432, 432, 4, 23, 4, 453, 45, 543]);
+
+// const repeatFn = (str) => (num) => {
+//   let result = str;
+//   for (let i = 0; i < num; i++) {
+//     result += result;
+//   }
+//   console.log(result);
+//   return result;
+// };
+
+// repeatFn("some")(4);
+
+const repeatFnTimes = (str) => (times) => {
+  let result = [];
+  for (let i = 0; i <= times; i++) {
+    result.push(str);
+  }
+  console.log(result.join().replace(/,/g, ""));
+  return result.join().replace(/,/g, "");
+};
+
+repeatFnTimes("some")(4);
