ecfe4254798bfbcc172c9cc330394baa94d1ce90
Alexander Makarof <makarofalex@gmail.com>
2020-04-27 08:19:01 +0300

    9 added string check

--- 9.js
+++ 9.js
@@ -1,11 +1,14 @@
 const findLongestStr = (str) => {
   let longestStr = 0;
-  str.split(" ").forEach((element) => {
-    if (longestStr < element.length) {
-      longestStr = element.length;
-      console.log(longestStr);
-    }
-  });
+  str
+    .split(" ")
+    .replace(/[^a-zA-Z]+/g, "")
+    .forEach((element) => {
+      if (longestStr < element.length) {
+        longestStr = element.length;
+        console.log(longestStr);
+      }
+    });
   return longestStr;
 };
 
