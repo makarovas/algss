47ea10d790d38df28e8258d6b629e9db32637792
Alexander Makarof <makarofalex@gmail.com>
2020-04-28 22:20:10 +0300

    added 10 _

--- 10.js
+++ 10.js
@@ -1,13 +1,30 @@
-const arrayFilterWithoutStrings = (array) => {
-  let result = [];
-  // let regExp = /[^a-zA-Z]+/g;
-  for (let i = 0; i < array.length; i++) {
-    if (!isNaN(array[i])) {
-      result.push(array[i]);
+// const arrayFilterWithoutStrings = (array) => {
+//   let resultStr = [];
+//   let resultNum = [];
+
+//   for (let i = 0; i < array.length; i++) {
+//     if (!isNaN(array[i])) {
+//       resultStr.push(array[i]);
+//     } else {
+//       resultNum.push(array[i]);
+//     }
+//   }
+//   console.log(resultStr, resultNum);
+//   return { resultStr, resultNum };
+// };
+
+// arrayFilterWithoutStrings(["sdklj2131", "dsfskldj", 23, 2342]);
+
+const res = (arr) => {
+  let filteredArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+    if (typeof arr[i] === "number") {
+      filteredArray.push(arr[i]);
     }
   }
-  console.log(result);
-  return result;
+
+  return filteredArray;
 };
 
-arrayFilterWithoutStrings(["sdklj2131", "dsfskldj", 23, 2342]);
+console.log(res(["2312kljl3kj21", 23, 3232, 324]));
